a9f5755d25b85df00597f4b82982a4f27f889522
Ben Cook <benthomascook28@gmail.com>
2017-06-16 17:35:53 -0400

    completed weather app draft

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,5 +1,4 @@
 	//declare global variables
-var location = $('#location');
 var percipMm = $('#percipMm');
 var percipIn = $('#percipIn');
 var temp = $('#temp');
@@ -8,108 +7,65 @@ var tempC = $('#tempC');
 
 var API_KEY = 'e5df8f57d5c207f6b49349e4ec590ae4';
 
+// var slideIndex =1;
+// showDivs(slideIndex);
+
+// function plusDivs(n) {
+//     showDivs(slideIndex += n);
+// }
+// function showDivs(n) {
+// 	var i;
+// 	var x = document.getElementsByClassName("slides");
+// 	if (n > x.length) {slideIndex = 1}
+// 	if (n < 1) {slideIndex = x.length} ;
+// 	for(i = 0; i < x.length; i++) {
+// 		x[i].style.display = "none";
+// 	}
+// 	x[slideIndex-1].style.display = "block";
+// }
 
 var placeTemplate = "" +
-	
-	"<p>Results for {{city}}, {{state}}</p>" +
-	"<p>Today there will be {{percipIn}} inches in rain.</p>" +
-	"<p>This means that there will be {{percipMm}} mm in rain today.</p>" +
+	'<div id="centerResults">' +
+	"<p>Results for {{name}}:</p>" +
+	"<p>Condition: {{description}}.</p>" +
 	"<p>It is {{tempF}} degrees ferenheit.</p>" +
-	"<p>This means it is {{tempC}} degrees celcius.</p>";
+	//"<p>It is "+ tempF + " degrees farenheit."</p>;
+	"<p>This means it is {{tempC}} degrees celcius.</p>" +
+	"</div>";
 
 //grab the entered friend, render it into a form HTML understands, attach this variable to the HTML document
 function addData(place){
 	$('#location').append(Mustache.render(placeTemplate, place));
 };
-		// var place = {
-		// 	city: $city.val(),
-		// 	state: $state.val(),
-		// 	percipIn: $percipIn.val(),
-		// 	percipCm: $percipCm.val(),
-		// };
+
 $(document).ready(function(){
 	$('#enter').on('click', function(){
 	var zipCode = $('#zipCode').val();
+	var weatherData = {};
 		$.ajax({
 			type: 'GET',
-			url: 'http://api.openweathermap.org/data/2.5/weather?zip=', zipCode+',us&apikey='+ API_KEY,
-			success: (function(fillIn){
-			console.log(fillIn);
-			$.each(fillIn.location, function(i, fillIn){
-			addData(place);
-			// addData(fillIn);
-			tempF = temp * 9/5-459.67;
-			tempC = temp - 273.15;
-			percipMm = rain;
-			percipIn = percipMm * .0393701;
-			});
+			url: 'http://api.openweathermap.org/data/2.5/weather?zip='+ zipCode+',us&apikey='+ API_KEY,
+		    success: (function(fillIn){
+
+			console.log(fillIn.name);
+			console.log("")
+			weatherData.name =fillIn.name;
+			weatherData.description = fillIn.weather[0].description;
+			weatherData.temp = fillIn.main.temp;
+			weatherData.tempF = (weatherData.temp * 9/5-459.67).toFixed(2);
+			weatherData.tempC = (weatherData.temp - 273.15).toFixed(2);
+
+			addData(weatherData);
+
 		}),
 			error: function(){
-				alert('error when filling in');
+				alert('please enter a valid zip code');
 			}
+
 		});
+		$("#centerResults").css({'text-align': 'center', 'font-size': '8em'});
 	});
+	// $("#clear").on('click', function(){
+	// 	$("#centerResults").empty();
+	// });
 });
-// 			}
-// 		})
-// 		})
-// 	});
-// });
-
-	// var API_KEY = '0befac3640104924a91cd10b4aece33b';
-	// $.ajax({
-	// 	url : "http://api.wunderground.com/api/"+API_KEY,
-	// 	dataType : "jsonp",
-	// 	success : function(parsed_json) {
-	// 	var location = parsed_json['location']['city'];
-	// 	var temp_f = parsed_json['current_observation']['temp_f'];
-	// 	alert("Current temperature in " + location + " is: " + temp_f);
-	// 	  }
-	// 	  });
-	// 	});
-		
-// 	$.ajax({
-// 		url : "http://api.wunderground.com/api/"+API_KEY+"conditions/q/"+$state+"/"+$city+".json"
-// 		dataType : "jsonp",
-// 		success : function(parsed_json) {
-// 		var location = parsed_json['location']['city'];
-// 		var temp_f = parsed_json['current_observation']['temp_f'];
-// 		alert("Current temperature in " + location + " is: " + temp_f);
-// 	};
-// });
-		
-//template the entered data will fill
-//grab the html page, when it loads, jQuery pulls from the API if successful, otherwise alerts with an error message
-// $(document).ready(function(){
-	// $.ajax({
-	// 	type: 'GET',
-	// 	url: 'http://api.wunderground.com/api/'+API_KEY+'conditions/q/'+$state+"/"+$city+'.json',
-	// 	success: function() {
-			// $.each(locations, function(i, place){
-			// 	addData(place);
-	// 		});
-	// 	},
-
-	// 	error: function(){
-	// 		alert('error getting data');
-	// 	}
-	//  });
-	// });
-
-	//when someone clicks the Add! button, adds the object the user provides to the table array in the API, otherwise error message
-	//.delegate allows you to remove items that were loaded by other students
-	// $friends.delegate('.remove', 'click', function(){
-
-	// 	var $li = $(this).closest('li');
-	// 	//AJAX DELETE Function - click the .remove class button and the id identifies what to delete
-	// 	$.ajax({
-	// 		type: 'DELETE',
-	// 		url: 'http://rest.learncode.academy/api/learncode/friends/' + $(this).attr('id'),
-	// 		success: function(){
-	// 			$li.fadeOut(300, function(){
-	// 				$(this).remove()
-	// 			});
-	// 		}
-	// 	});
-	// });
-// });
